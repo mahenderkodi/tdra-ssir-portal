99f1095b8e2f3ce8467b0f3d6ac6f272d97c210d
kodimahender55@gmail.com <mahenderkabali0001@gmail.com>
2026-07-18 09:51:16 +0530

    add: Backend custom modules directory with ssir.core,  ssir.api

---
+++ backend/ssir-api/src/main/java/ae/gov/tdra/ssir/api/SSIRApplication.java
@@ -0,0 +1,19 @@
+package ae.gov.tdra.ssir.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "ae.gov.tdra.ssir")
+@EnableJpaAuditing
+// Explicitly scans core package for databases repositories
+@EnableJpaRepositories(basePackages = "ae.gov.tdra.ssir.core.repository")
+// Explicitly scans core package for entities
+@EntityScan(basePackages = "ae.gov.tdra.ssir.core.entity")
+public class SSIRApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSIRApplication.class, args);
+    }
+}
