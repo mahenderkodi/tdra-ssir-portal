79326bac4c722324d7e6a23755a08c87f12a90c2
kodimahender55@gmail.com <mahenderkabali0001@gmail.com>
2026-07-18 08:25:59 +0530

    add: custom modules directory with common lib, sender-registry, and role-login app

---
+++ frontend/modules/common/src/lib/common.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-common',
+  imports: [],
+  template: ` <p>common works!</p> `,
+  styles: ``,
+})
+export class Common {}
